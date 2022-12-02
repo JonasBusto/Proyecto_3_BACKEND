dbddfea8b89c308941e1d03741d09fc5224e015f
Jonas Busto <jonasrex00@gmail.com>
2022-12-02 01:19:40 -0300

    db url agregado

--- dataBase.js
+++ dataBase.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect(
-//   "mongodb+srv://JonasBustoRCS:Vk2YEqjIgDwtaKuJ@proyecto3grupo4rcs.skunrq6.mongodb.net/?retryWrites=true&w=majority"
-// );
+mongoose.connect(
+  "mongodb+srv://JonasBustoRCS:Vk2YEqjIgDwtaKuJ@proyecto3grupo4rcs.skunrq6.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.MONGODB_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
