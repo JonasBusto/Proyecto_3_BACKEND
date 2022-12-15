2fef7900ea0942a3ac624b460a7ff1c9ea906009
Jonas Busto <jonasrex00@gmail.com>
2022-12-15 20:09:16 -0300

    refactorizando

--- dataBase.js
+++ dataBase.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 mongoose.connect(
   "mongodb+srv://JonasBustoRCS:Vk2YEqjIgDwtaKuJ@proyecto3grupo4rcs.skunrq6.mongodb.net/?retryWrites=true&w=majority",
   {
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 const routesUser = require("./routes/userRoutes");
 const routesPlaces = require("./routes/placeRouter");
 const routesProvince = require("./routes/provinceRoutes");
